ef7c8ef79e7d20beddd7bed62783f397f239968a
Nero <neroshan12@gmail.com>
2017-12-30 18:57:17 +0000

    Add bitcoin price in USD rendering in command line

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+global.fetch = require('node-fetch');
+const cc = require('cryptocompare');
 
 app.listen(3000, function() {
   console.log('listening on 3000');
@@ -7,4 +9,17 @@ app.listen(3000, function() {
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+  // cc
+  //   .coinList()
+  //   .then(coinList => {
+  //     console.log(coinList[0]);
+  //   })
+  //   .catch(console.error);
+
+  cc.price('BTC', ['USD']).then(prices => {
+    //console.log(prices);
+    // -> { USD: 1100.24, EUR: 1039.63 }
+    var BTCprice = prices;
+    console.log(BTCprice.USD);
+  });
 });
